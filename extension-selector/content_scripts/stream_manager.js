182bae45a640c1b1c64b81d098f5365ada58f7ef
Edward Lee <edwardlee.259@gmail.com>
2020-11-26 19:43:19 -0800

    add eslint and fix issues (kinda)

--- extension-selector/content_scripts/stream_manager.js
+++ extension-selector/content_scripts/stream_manager.js
@@ -1,118 +1,119 @@
 (() => {
-	if (window.hasRun) {
-		return;
-	}
-	window.hasRun = true;
+  if (window.hasRun) {
+    return;
+  }
+  window.hasRun = true;
 
-	let myId = null;
-	let someIndex = 0;
+  let myId = null;
+  let someIndex = 0;
 
-	const getStreamsForTab = (tabId) => browser.storage.local.get(tabId);
+  const getStreamsForTab = (tabId) => browser.storage.local.get(tabId);
 
-	const findMediaElement = (el, parentDepth = 2) => {
-		for (i = 0; i < parentDepth; i++) {
-			el = el.parentNode;
-		}
-		return el.querySelector('audio') || el.querySelector('video');
-	};
+  const findMediaElement = (el, parentDepth = 2) => {
+    let myEl = el;
+    for (let i = 0; i < parentDepth; i += 1) {
+      myEl = myEl.parentNode;
+    }
+    return el.querySelector('audio') || el.querySelector('video');
+  };
 
-	// These aren't atomic, but whatever
-	const mediaElementCSS = `
-		video {
-			border: 5px solid red;
-		}
-		audio {
-			border: 5px solid red;
-		}
-	`;
-	const chooseStream = (index) => {
-		console.log('choosing stream');
-		const styleElement = document.createElement('style');
-		styleElement.innerHTML = mediaElementCSS;
+  // These aren't atomic, but whatever
+  const mediaElementCSS = `
+  video {
+    border: 5px solid red;
+  }
+  audio { 
+    border: 5px solid red; 
+  }`;
+  const chooseStream = (index) => {
+    console.log('choosing stream');
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = mediaElementCSS;
 
-		document.head.appendChild(styleElement);
-		console.log("injected CSS");
+    document.head.appendChild(styleElement);
+    console.log('injected CSS');
 
-		return new Promise((resolve, reject) => {
-			const listener = (e) => {
-				e.preventDefault();
-				e.stopPropagation();
-				teardown();
-				mediaElement = findMediaElement(e.target);
-				if (mediaElement) {
-					resolve({
-						id: index,
-						element: mediaElement.outerHTML,
-					});
-				} else {
-					reject();
-				}
-			}
+    return new Promise((resolve, reject) => {
+      const listener = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        teardown();
+        const mediaElement = findMediaElement(e.target);
+        console.log(mediaElement);
+        if (mediaElement) {
+          resolve({
+            id: index,
+            element: mediaElement.outerHTML,
+          });
+        } else {
+          reject();
+        }
+      };
 
-			const teardown = () => {
-				console.log('teardown');
-				window.removeEventListener('blur', teardown);
-				window.removeEventListener('click', listener, { capture: true });
-				document.head.removeChild(styleElement);
-			}
-			window.addEventListener('click', listener, { capture: true });
-			window.addEventListener('blur', teardown);
-		});
-	};
+      const teardown = () => {
+        console.log('teardown');
+        window.removeEventListener('blur', teardown);
+        window.removeEventListener('click', listener, { capture: true });
+        document.head.removeChild(styleElement);
+      };
 
-	const addStream = async (tabId, index) => {
-		console.log("adding stream");
-		stream = await chooseStream(index);
+      window.addEventListener('click', listener, { capture: true });
+      window.addEventListener('blur', teardown);
+    });
+  };
 
-		console.log('adding to stream', tabId, stream);
-		getStreamsForTab(tabId)
-			.then(streams => {
-				updated = streams[tabId] || [];
-				updated.push(stream);
-				streams[tabId] = updated;
-				browser.storage.local.set(streams);
-			})
-			.catch(e => console.log('failed adding to stream', e));
-		console.log('test');
-	};
+  const addStream = async (tabId, index) => {
+    console.log('adding stream');
+    const stream = await chooseStream(index);
 
-	const deleteStream = async (tabId, streamId) => {
-		console.log('deleting stream', tabId, streamId);
-		streams = await getStreamsForTab(tabId);
-		updated = streams[tabId] || [];
-		console.log(updated);
-		updated = updated.filter(stream => stream.id !== streamId);
-		console.log(updated);
-		if (updated.length === 0) {
-			browser.storage.local.remove(tabId);
-		} else {
-			streams[tabId] = updated;
-			browser.storage.local.set(streams);
-		}
-	};
+    console.log('adding to stream', tabId, stream);
+    getStreamsForTab(tabId)
+      .then((streams) => {
+        const updated = streams[tabId] || [];
+        updated.push(stream);
+        const updatedStreams = streams;
+        updatedStreams[tabId] = updated;
+        browser.storage.local.set(updatedStreams);
+      })
+      .catch((e) => console.log('failed adding to stream', e));
+    console.log('test');
+  };
 
-	const deleteAll = (tabId) => {
-		console.log('deleting all', tabId);
-		browser.storage.local.remove(tabId);
-	};
+  const deleteStream = async (tabId, streamId) => {
+    console.log('deleting stream', tabId, streamId);
+    const streams = await getStreamsForTab(tabId);
+    let updated = streams[tabId] || [];
+    updated = updated.filter((stream) => stream.id !== streamId);
+    if (updated.length === 0) {
+      browser.storage.local.remove(tabId);
+    } else {
+      streams[tabId] = updated;
+      browser.storage.local.set(streams);
+    }
+  };
 
-	const messageHandler = (message) => {
-		console.log(message);
-		if (message.command === 'chooser-start') {
-			myId = message.id;
-			someIndex += 1;
-			addStream(myId, someIndex);
-		} else if (message.command === 'stop-all') {
-			deleteAll(myId || message.id);
-		} else if (message.command === 'stop-id') {
-			deleteStream(myId || message.id, message.streamId);
-		}
-	};
+  const deleteAll = (tabId) => {
+    console.log('deleting all', tabId);
+    browser.storage.local.remove(tabId);
+  };
 
-	browser.runtime.onMessage.addListener(messageHandler);
+  const messageHandler = (message) => {
+    console.log(message);
+    if (message.command === 'chooser-start') {
+      myId = message.id;
+      someIndex += 1;
+      addStream(myId, someIndex);
+    } else if (message.command === 'stop-all') {
+      deleteAll(myId || message.id);
+    } else if (message.command === 'stop-id') {
+      deleteStream(myId || message.id, message.streamId);
+    }
+  };
 
-	window.onbeforeunload = () => {
-		browser.runtime.onMessage.removeListener(messageHandler);
-		deleteAll(myId);
-	};
+  browser.runtime.onMessage.addListener(messageHandler);
+
+  window.onbeforeunload = () => {
+    browser.runtime.onMessage.removeListener(messageHandler);
+    deleteAll(myId);
+  };
 })();
--- extension-selector/popup/popup.js
+++ extension-selector/popup/popup.js
@@ -1,106 +1,103 @@
-const getStreams = async () => await browser.storage.local.get();
+const getStreams = async () => browser.storage.local.get();
 
 const renderStreamList = (streams) => {
-	const container = document.getElementById('elements-list-container');
-	container.innerHTML = ''; // clear children of the list
+  const container = document.getElementById('elements-list-container');
+  container.innerHTML = ''; // clear children of the list
 
-	tabIds = Object.keys(streams);
-	console.log(tabIds);
-	if (tabIds.length > 0) {
-		const elementsList = document.createElement('ul');
-		tabIds.forEach(tabId => {
-			streams[tabId].forEach(stream => {
-				const entry = document.createElement('li');
-				entry.innerText = `tab: ${tabId} stream: ${stream.id}`;
+  const tabIds = Object.keys(streams);
+  console.log(tabIds);
+  if (tabIds.length > 0) {
+    const elementsList = document.createElement('ul');
+    tabIds.forEach((tabId) => {
+      streams[tabId].forEach((stream) => {
+        const entry = document.createElement('li');
+        entry.innerText = `tab: ${tabId} stream: ${stream.id}`;
 
-				const deleteButton = document.createElement('button');
-				deleteButton.innerText = "delete";
-				deleteButton.addEventListener('click', () => {
-					browser.tabs.sendMessage(parseInt(tabId), {
-						command: 'stop-id',
-						id: tabId,
-						streamId: stream.id,
-					}).catch(e =>
-						console.log('failed to stop: ', tabId, stream.id, e)
-					);
-				});
-				entry.appendChild(deleteButton);
-				elementsList.appendChild(entry);
-			});
-		});
-		container.appendChild(elementsList);
-	} else {
-		container.innerText = 'No streams exist at this time.';
-	}
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = 'delete';
+        deleteButton.addEventListener('click', () => {
+          browser.tabs.sendMessage(parseInt(tabId, 10), {
+            command: 'stop-id',
+            id: tabId,
+            streamId: stream.id,
+          }).catch((e) => console.log('failed to stop: ', tabId, stream.id, e));
+        });
+        entry.appendChild(deleteButton);
+        elementsList.appendChild(entry);
+      });
+    });
+    container.appendChild(elementsList);
+  } else {
+    container.innerText = 'No streams exist at this time.';
+  }
 };
 
 const storageChangeHandler = (changes, areaName) => {
-	if (areaName === 'local') {
-		// re-render list of things
-		getStreams().then(renderStreamList);
-	}
+  if (areaName === 'local') {
+    // re-render list of things
+    getStreams().then(renderStreamList);
+  }
 };
 
-const clickHandler = (e) => {
-	const onClickAction = () => {
-		console.log('clicked action');
-		if (e.target.classList.contains('action-chooser')) {
-			// we want to start the element chooser process in the current tab
-			browser.tabs.query({ active: true, currentWindow: true })
-				.then(myTab => {
-					browser.tabs.sendMessage(myTab[0].id, {
-						command: 'chooser-start',
-						id: myTab[0].id.toString(),
-					})
-						.catch(e => console.log('failed to send to:', myTab[0].id, e))
-				});
-		}
-		else if (e.target.classList.contains('action-clear')) {
-			// clear all tracked streams
-			browser.tabs.query({}).then(allTabs => {
-				allTabs.forEach(tab => {
-					browser.tabs.sendMessage(tab.id, {
-						command: 'stop-all',
-						id: tab.id.toString(),
-					}).catch(e => console.log('failed to send to:', tab.id, e));
-				});
-			});
-		}
-	}
+const clickHandler = (ev) => {
+  const onClickAction = () => {
+    console.log('clicked action');
+    if (ev.target.classList.contains('action-chooser')) {
+      // we want to start the element chooser process in the current tab
+      browser.tabs.query({ active: true, currentWindow: true })
+        .then((myTab) => {
+          browser.tabs.sendMessage(myTab[0].id, {
+            command: 'chooser-start',
+            id: myTab[0].id.toString(),
+          })
+            .catch((e) => console.log('failed to send to:', myTab[0].id, e));
+        });
+    } else if (ev.target.classList.contains('action-clear')) {
+      // clear all tracked streams
+      browser.tabs.query({}).then((allTabs) => {
+        allTabs.forEach((tab) => {
+          browser.tabs.sendMessage(tab.id, {
+            command: 'stop-all',
+            id: tab.id.toString(),
+          }).catch((e) => console.log('failed to send to:', tab.id, e));
+        });
+      });
+    }
+  };
 
-	if (e.target.classList.contains('action')) {
-		// get all tabs in case we need to clear our listening to a certain tab
-		onClickAction();
-	}
+  if (ev.target.classList.contains('action')) {
+    // get all tabs in case we need to clear our listening to a certain tab
+    onClickAction();
+  }
 };
 
 let isListening = false;
 const addListeners = () => {
-	if (isListening) {
-		return;
-	}
-	isListening = true;
+  if (isListening) {
+    return;
+  }
+  isListening = true;
 
-	document.addEventListener('click', clickHandler);
-	browser.storage.onChanged.addListener(storageChangeHandler);
-	window.onblur = () => {
-		browser.storage.onChanged.removeListener(storageChangeHandler);
-	}
-}
+  document.addEventListener('click', clickHandler);
+  browser.storage.onChanged.addListener(storageChangeHandler);
+  window.onblur = () => {
+    browser.storage.onChanged.removeListener(storageChangeHandler);
+  };
+};
 
 const startup = () => {
-	getStreams().then(renderStreamList);
-	addListeners();
+  getStreams().then(renderStreamList);
+  addListeners();
 };
 
 let executed = false;
 (() => {
-	if (executed) return;
-	executed = true;
+  if (executed) return;
+  executed = true;
 
-	console.log('executing script');
+  console.log('executing script');
 
-	browser.tabs.executeScript({ file: '/content_scripts/stream_manager.js' })
-		.then(startup)
-		.catch(e => console.log('there was some error in executing', e));
+  browser.tabs.executeScript({ file: '/content_scripts/stream_manager.js' })
+    .then(startup)
+    .catch((e) => console.log('there was some error in executing', e));
 })();
